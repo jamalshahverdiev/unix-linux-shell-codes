1e7fede78696b6f1e5b9e4cbe05eb40fb34c7345
Jamal Shahverdiyev <camal.sahverdiyev@kapitalbank.az>
2020-02-12 15:44:24 +0400

    Added new code

---
+++ Jenkins-Scripts/code1.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE:  code1.sh
+# 
+#         USAGE:  ./code1.sh 
+# 
+#   DESCRIPTION:  :
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Jamal Shahverdiev (), jamal.shahverdiev@gmail.com
+#       COMPANY:  KapitalBank LLC
+#       VERSION:  1.0
+#       CREATED:  02/12/2020 03:44:08 PM +04
+#      REVISION:  ---
+#===============================================================================
+
+
