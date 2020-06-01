bb7144c26b35c693ddb252a8508d38f9efa4aa51
Seymur Huseynli <seymurhus@youforce.net>
2020-06-01 22:09:31 +0300

    .net core solution

---
+++ IPCheck/IP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCheck
+{
+    public class IP : IComparable
+    {
+        public IP(string line)
+        {
+            string[] values = line.Split(' ');
+            Address = values[0];
+            Status = values[1];
+            string[] ips = Address.Split('.');
+            P1 = Convert.ToInt32(ips[0]);
+            P2 = Convert.ToInt32(ips[1]);
+            P3 = Convert.ToInt32(ips[2]);
+            P4 = Convert.ToInt32(ips[3]);
+        }
+
+        int P1 { get; set; }
+
+        int P2 { get; set; }
+
+        int P3 { get; set; }
+
+        int P4 { get; set; }
+
+        public string Address { get; set; }
+
+        public string Status { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            IP comp = (IP)obj;
+            int result = P1.CompareTo(comp.P1);
+            if (result != 0) return result;
+            result = P2.CompareTo(comp.P2);
+            if (result != 0) return result;
+            result = P3.CompareTo(comp.P3);
+            if (result != 0) return result;
+            result = P4.CompareTo(comp.P4);
+            return result;
+        }
+    }
+}
---
+++ IPCheck/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace IPCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var ipValues = File.ReadAllLines(@"C:\CV\source.txt");
+            List<IP> IPs = new List<IP>();
+            foreach(string value in ipValues)
+            {
+                IPs.Add(new IP(value));
+            }
+
+            IPs.Sort();
+            StringBuilder builder = new StringBuilder();
+            int open = 0;
+            int closed = 0;
+            IP last = IPs[0];
+
+            for (int i = 1;i< IPs.Count; i++)
+            {
+                
+                if(last.Address != IPs[i].Address)
+                {
+                    builder.AppendLine($"{last.Address} open:{open} closed:{closed}");
+                    last = IPs[i];
+                    open = 0;
+                    closed = 0;
+                }
+
+                if(IPs[i].Status[0] == 'o')
+                {
+                    open++;
+                }
+                else
+                {
+                    closed++;
+                }
+            }
+
+            File.WriteAllText(@"C:\CV\results.txt", builder.ToString());
+            watch.Stop();
+            
+            Console.WriteLine(watch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+}
